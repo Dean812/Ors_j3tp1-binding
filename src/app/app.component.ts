e27be278246e122246327ee2af6dc52eb5939cc9
Thibaut <tibeloute@msn.com>
2022-08-02 10:28:05 +0200

    starting binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my component : tp-composant';
+  hero= 'spiderman'
 }
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
 
+  @Input() name= "default";
   constructor() { }
 
   ngOnInit(): void {
